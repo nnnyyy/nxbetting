d62a7280e72a84a8396d10ff5fe58acde8f37bf8
Unknown <wm99sdw@nate.com>
2018-04-12 18:34:08 +0900

    쿼리수정

--- Document/battable_add.sql
+++ Document/battable_add.sql
@@ -1,27 +1,24 @@
+
+#유저 배팅관련
 delimiter //
 create procedure bettable_add
 (in 
-accountid int(11),
-competitionSN int(11) , #경기sn
-betside tinyint(1), #유저선택 1:홈승, 2:무승부, 3:원정승
-betmoney int(11) #배팅금액
+accountid_in int(11),
+competitionSN_in int(11) , #경기sn
+betside_in tinyint(1), #유저선택 1:홈승, 2:무승부, 3:원정승
+betmoney_in int(11) #배팅금액
 )
 begin 
-	declare A int(11);
-	declare State tinyint(1);
-	
-	select State into State from CompetitionList where sn = competitionSN;
-	select assets into A from Account_Assets where accountid = accountid;
-if	state = 1 then #경기전일때true
 
-	if A >= betmoney then #베팅머니가 보유머니보다 작거나같을때true
+	select @State_out := State from CompetitionList where sn = competitionSN_in;
+	select @assets_out := Assets from Account_Assets where accountid = accountid_in;
+    
+if	@State_out = 1 and @assets_out >= betmoney_in then #경기전일때true #베팅머니가 보유머니보다 작거나같을때true
 		update Account_Assets
-		set assets = assets-betmoney
-		where accountid = accountid;
+		set assets = assets - betmoney_in
+		where accountid = accountid_in;
 	insert into bettable (competitionSN,accountid,betside,betmoney,regdate)
-	values (competitionSN,accountid,betside, betmoney, now());
-	else select 1;
-	end if;
+	values (competitionSN_in,accountid_in,betside_in, betmoney_in, now());
 else select 0;
 end if;
 
