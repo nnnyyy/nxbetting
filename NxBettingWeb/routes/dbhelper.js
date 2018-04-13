1e534e1b40bc6a4ad88a7fa7da720c0b5215db7f
nnnyyy <nnnyyy@nate.com>
2018-04-13 13:43:03 +0900

    게임 화면

--- NxBettingWeb/routes/dbhelper.js
+++ NxBettingWeb/routes/dbhelper.js
@@ -22,9 +22,9 @@ exports.GetList = function(cb) {
     data = {}
 
     data.list = [
-        {sn: 0, home: 'www', away: 'kkk', home_rate: 187, away_rate: 174, regdate: '2018-04-13 6:00:00', betlimitdate: '2018-04-13 7:00:00', state: 2 },
-        {sn: 1, home: 'www', away: 'kkk', home_rate: 187, away_rate: 174, regdate: '2018-04-13 10:00:00',betlimitdate: '2018-04-13 7:00:00', state: 0 },
-        {sn: 2, home: 'www', away: 'kkk', home_rate: 187, away_rate: 174, regdate: '2018-04-13 10:00:00', betlimitdate: '2018-04-13 7:00:00', state: 0 }
+        {sn: 0, home: '장민석', away: '문희대', home_rate: 187, away_rate: 174, regdate: '2018-04-13 6:00:00', betlimitdate: '2018-04-13 7:00:00', state: 2 },
+        {sn: 1, home: '신동휘', away: '왕예식', home_rate: 187, away_rate: 174, regdate: '2018-04-13 10:00:00',betlimitdate: '2018-04-13 7:00:00', state: 0 },
+        {sn: 2, home: '신동규', away: '박건희', home_rate: 187, away_rate: 174, regdate: '2018-04-13 10:00:00', betlimitdate: '2018-04-13 7:00:00', state: 0 }
     ];
     cb(data);
 }
